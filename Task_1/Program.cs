0059e0d88f05e502dcb679a3f9412b32bec75d80
NGShpagin <shpagin9393@mail.ru>
2022-06-05 11:28:25 +0300

    first commit

---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1.
+// Задачйте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+
+void ValueSet(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num}, ");
+    ValueSet(num-1);
+}
+
+ValueSet(5);
+Console.WriteLine();
---
+++ Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 3. 
+// Напишите программу, которая будет принимать на вход число
+// и возвращать сумму его цифр. Использовать рекурсию.
+
+int ValueSet(int num)
+{
+    if (num == 0) return 0;
+    return ValueSet(num / 10) + num % 10;
+
+}
+
+Console.Write(ValueSet(1234));
+Console.WriteLine();
